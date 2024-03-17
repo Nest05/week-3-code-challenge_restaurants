e832acc53f0d734b2976e9c66b5488b7b5a2497d
Nestor Masinde <nestormasinde@gmail.com>
2024-03-17 18:51:46 +0300

    feat: create db and add data to table

--- restaurant_reviews.py
+++ restaurant_reviews.py
@@ -18,14 +18,14 @@ class Customer(Base):
     __tablename__ = 'customers'
 
     id = Column(Integer, primary_key=True, nullable=False)
-    first_name = Column(String, unique=True, nullable=False)
+    first_name = Column(String, nullable=False)
     last_name = Column(String, unique=True, nullable=False)
 
 class Review(Base):
     __tablename__ = 'reviews'
 
-    restaurant_id = Column(Integer, ForeignKey('restaurants_id'), primary_key=True, nullable=False)
-    customer_id = Column(Integer, ForeignKey('customers_id'), primary_key=True, nullable=False)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'), primary_key=True, nullable=False)
+    customer_id = Column(Integer, ForeignKey('customers.id'), primary_key=True, nullable=False)
     star_rating = Column(Integer, nullable=False)
 
     __table_args__ = (
@@ -51,3 +51,53 @@ class ReviewManagementSystem:
             self.session.add(restaurant)
             self.session.commit()
             print("Restaurant Added successfully")
+        except Exception as e:
+            self.session.rollback()
+            print(f'Error: {e}')
+
+    def add_customer(self, first_name, last_name):
+        
+        if not first_name or not last_name:
+            print("Error: fields cannot be empty")
+        
+        customer = Customer(first_name=first_name, last_name=last_name)
+
+        try:
+            self.session.add(customer)
+            self.session.commit()
+            print("Customer Added Successfully")
+        except Exception as e:
+            self.session.rollback()
+            print(f'Error: {e}')
+
+    def associate_review(self, restaurant_id, customer_id, star_rating):
+        # if not restaurant_id or not customer_id or not star_rating:
+        #     print("Error: fields cannot be empty")
+
+        review = Review(restaurant_id=restaurant_id, customer_id=customer_id, star_rating=star_rating)
+
+        try:
+            self.session.add(review)
+            self.session.commit()
+            print("Review Successfully added")
+        except Exception as e:
+            self.session.rollback()
+            print(f'Error: {e}')
+
+
+if __name__ == '__main__':
+    restaurant_review_management = ReviewManagementSystem("restaurant_review_manager.db")
+
+    # restaurant_review_management.add_restaurant('Jajamelo', 500)
+    # restaurant_review_management.add_restaurant('Mama Rocks', 1000)
+    # restaurant_review_management.add_restaurant('Urban Burger', 1200)
+
+    # restaurant_review_management.add_customer('Nestor', 'Masinde')
+    # restaurant_review_management.add_customer('John', 'Kimani')
+    # restaurant_review_management.add_customer('Levis', 'Ngige')
+    # restaurant_review_management.add_customer('Naomi', 'Lagat')
+    # restaurant_review_management.add_customer('John', 'Ouma')
+    # restaurant_review_management.add_customer('Monica', 'Mwangi')
+
+    restaurant_review_management
+    
