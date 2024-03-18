622251d54e089a9e684f0860b011c1fa0d9ba62c
Nestor Masinde <nestormasinde@gmail.com>
2024-03-18 18:09:51 +0300

    feat: use class in main.py

--- models/customer.py
+++ models/customer.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base(1)
 
 class Customer(Base):
@@ -11,20 +12,26 @@ class Customer(Base):
     first_name = Column(String, nullable=False)
     last_name = Column(String, unique=True, nullable=False)
 
-    def reviews():
-        pass
+    reviews = relationship('review.Review', backref='customer')
+    restaurants = relationship('restaurant.Restaurant', secondary = 'review.reviews' )
 
-    def restaurants():
-        pass
+    def reviews(self):
+        return self.reviews
 
-    def full_name():
-        pass
+    def restaurants(self):
+        return self.restaurants
 
-    def favorite_retsaurant():
-        pass
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
-    def add_review(restaurant, rating):
-        pass
+    def favorite_restaurant(self):
+        return max(self.restaurants, key=lambda restaurant: restaurant.star_rating)
 
-    def delete_reviews(restaurant):
-        pass    
+    def add_review(self, restaurant, rating):
+        from review import Review
+        review = Review(restaurant_name=restaurant.name, rating=rating)
+        self.reviews.append(review)
+
+    def delete_reviews(self, restaurant):
+        self.reviews = [review for review in self.reviews if review.restaurant_name != restaurant.name]
+           
--- models/main.py
+++ models/main.py
@@ -5,13 +5,24 @@ from customer import Base as CustomerBase, Customer
 from restaurant import Base as RestaurantBase, Restaurant
 from review import Base as ReviewBase, Review
 
-engine = create_engine('sqlite:///restaurant_review_manager.db')
-Session = sessionmaker(bind=engine)
-session = Session()
+# engine = create_engine('sqlite:///restaurant_review_manager.db')
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
-CustomerBase.metadata.create_all(engine)
-RestaurantBase.metadata.create_all(engine)
-ReviewBase.metadata.create_all(engine)
+# CustomerBase.metadata.create_all(engine)
+# RestaurantBase.metadata.create_all(engine)
+# ReviewBase.metadata.create_all(engine)
 
 
+class ReviewManagementSystem:
 
+    def __init__(self, db_name):
+        self.engine = create_engine(f'sqlite:///{db_name}')
+        CustomerBase.metadata.create_all(self.engine)
+        RestaurantBase.metadata.create_all(self.engine)
+        ReviewBase.metadata.create_all(self.engine)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+
+if __name__ == '__main__':
+    restaurant_review_management = ReviewManagementSystem("restaurant_review_manager.db")
